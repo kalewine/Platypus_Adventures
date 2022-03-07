3b866a37a1f7bc92971ec97a1e2903c43ae6459a
kalewine <kayla.r.alewine@gmail.com>
2022-03-07 15:41:30 -0500

    Finish navbar dropdowns

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,9 +2,20 @@ const toggleBtn = document.getElementById('toggle-btn');
 const logo = document.getElementById('logo');
 const arch = document.getElementById('small-arch');
 const navmenu = document.getElementById('nav-menu');
-const navlist = document.getElementById('nav-list')
+const navlist = document.getElementById('nav-list');
+const navListItem = document.getElementsByClassName('nav__list-item');
+const navListLink = document.getElementsByClassName('nav__list-link');
+const navDropdown = document.getElementsByClassName('nav__dropdown');
+const dropdownToggle = document.getElementsByClassName('nav__dropdown-toggle-icon');
+
+const closeDropdowns = () => {
+    Array.from(dropdownToggle).forEach(icon => icon.classList.remove('is-open'));
+    Array.from(navDropdown).forEach(dropdown => dropdown.classList.remove('is-open'));
+}
 
 const toggleMenu = () => {
+    closeDropdowns()
+    Array.from(navListItem).forEach(item => item.classList.toggle('is-active'))
     toggleBtn.classList.toggle('is-active');
     logo.classList.toggle('is-active');
     navmenu.classList.toggle('is-active')
@@ -17,4 +28,19 @@ const toggleMenu = () => {
     }
 }
 
+
+const toggleDropdown = (toggleIcon, id) => {
+    toggleIcon.children[0].classList.toggle('is-open')
+    document.getElementById(id).classList.toggle('is-open');
+    Array.from(navDropdown).forEach(item => {
+        if(item.id !== id){
+            item.classList.remove('is-open')
+        }
+    })
+    Array.from(dropdownToggle).forEach(icon => {
+        if(icon !== toggleIcon.children[0]){
+            icon.classList.remove('is-open')
+        }
+    })
+}
 
