a2e3c962c66dc9fb8e1e8fd387409290d03a0e3e
kalewine <kayla.r.alewine@gmail.com>
2022-03-17 16:42:19 -0400

    Navbar on scroll / responsive footer and adventures section

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,37 +1,84 @@
 const toggleBtn = document.getElementById('toggle-btn');
+const target = document.getElementById('target');
+const body = document.querySelector('body');
 const logo = document.getElementById('logo');
-const arch = document.getElementById('small-arch');
+const shadowLogo = document.getElementById('shadow-logo');
+const desktopDonate = document.getElementById('desktop-donate-btn');
 const navmenu = document.getElementById('nav-menu');
 const navlist = document.getElementById('nav-list');
 const navListItem = document.getElementsByClassName('nav__list-item');
 const navListLink = document.getElementsByClassName('nav__list-link');
 const navDropdown = document.getElementsByClassName('nav__dropdown');
+const navDropdownLink = document.getElementsByClassName('nav__dropdown-link');
+const workShadow = document.getElementById('work-shadow');
+const involvedShadow = document.getElementById('involved-shadow');
+const aboutShadow = document.getElementById('about-shadow');
+
 const dropdownToggle = document.getElementsByClassName('nav__dropdown-toggle-icon');
 
+// navbar shink/grow on scroll
+const addScrolled = [logo, shadowLogo, desktopDonate, workShadow, involvedShadow, aboutShadow]
+
+const scrolledOptions = {
+    threshold: 1.0
+}
+
+const resizeNavbar = new IntersectionObserver ((entries, resizeNavbar) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            addScrolled.forEach(element=> {
+                element.classList.remove('scrolled')
+            })
+            Array.from(navListLink).forEach(element => {
+                element.classList.remove('scrolled')
+            })
+            Array.from(navListItem).forEach(element => {
+                element.classList.remove('scrolled')
+            })
+            Array.from(navDropdownLink).forEach(element => {
+                element.classList.remove('scrolled')
+            })
+        }else if (!entry.isIntersecting){
+            addScrolled.forEach(element=> {
+                element.classList.add('scrolled')
+            })
+            Array.from(navListLink).forEach(element => {
+                element.classList.add('scrolled')
+            })
+            Array.from(navListItem).forEach(element => {
+                element.classList.add('scrolled')
+            })
+            Array.from(navDropdownLink).forEach(element => {
+                element.classList.add('scrolled')
+            })
+        }
+    })
+}, scrolledOptions)
+
+resizeNavbar.observe(target)
+
+// close mobile menu dropdowns
 const closeDropdowns = () => {
     Array.from(dropdownToggle).forEach(icon => icon.classList.remove('is-open'));
     Array.from(navDropdown).forEach(dropdown => dropdown.classList.remove('is-open'));
 }
 
+// mobile menu toggle
 const toggleMenu = () => {
     closeDropdowns()
     Array.from(navListItem).forEach(item => item.classList.toggle('is-active'))
     toggleBtn.classList.toggle('is-active');
     logo.classList.toggle('is-active');
+    shadowLogo.classList.toggle('is-active');
     navmenu.classList.toggle('is-active')
     navlist.classList.toggle('is-active')
-    arch.classList.toggle('is-active');
-    if(arch.classList.contains("is-active")){
-        arch.setAttribute('d','M 0 0 H 640 V 125 C 125 125 125 125 0 125 V 0 Z')
-    }else {
-        arch.setAttribute('d','M 0 0 H 640 V 125 C 405 125 150 100 0 275 V 0 Z')
-    }
+    body.classList.toggle('is-active')
 }
 
-
+// toggle mobile menu dropdowns
 const toggleDropdown = (toggleIcon, id) => {
     toggleIcon.children[0].classList.toggle('is-open')
-    document.getElementById(id).classList.toggle('is-open');
+    document.getElementById(id ).classList.toggle('is-open')
     Array.from(navDropdown).forEach(item => {
         if(item.id !== id){
             item.classList.remove('is-open')
