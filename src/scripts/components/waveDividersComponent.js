95b627c3a7061e6258827d270998d8a1f8a79c7f
kalewine <kayla.r.alewine@gmail.com>
2022-03-22 17:40:11 -0400

    Make adaptable components

--- src/scripts/components/waveDividersComponent.js
+++ src/scripts/components/waveDividersComponent.js
@@ -1,87 +1,87 @@
-class DonateGreenPurple extends HTMLElement {
-    connectedCallback(){
-        this.innerHTML = 
-        `<section class="waves__container">
-        <img class="waves__crests top-waves" src="../../images/green-waves-top.svg" alt="upward wave crests"> 
-        <div class="waves__body waves__green">
-            <div class="divider__content">
-                <h2 class="divider__text">Help Make the Adventures Happen</h2>
-                <a class="button button__purple" href="https://paypal.me/platypusadventures?country.x=US&locale.x=en_US">Donate</a>   
-            </div>
-        </div>
-        <img class="waves__crests bottom-waves" src="../../images/green-waves-bottom.svg" alt="downward wave crests"> 
-    </section>`
-    }
-}
+class IntroWaves extends HTMLElement {
+    constructor(){
+        super();
+        
+        const introSection = document.createElement('section');
+        introSection.setAttribute('class', 'waves__container');
+        introSection.setAttribute('id', 'waves-intro');
+        const wavesTopCrests = document.createElement('img');
+        wavesTopCrests.setAttribute('class', 'waves__crests top-waves');
+        wavesTopCrests.setAttribute('src', '../../images/purple-waves-top.svg');
+        wavesTopCrests.setAttribute('alt', 'upward wave crests');
+        const wavesBottomCrests = document.createElement('img');
+        wavesBottomCrests.setAttribute('class', 'waves__crests bottom-waves');
+        wavesBottomCrests.setAttribute('src', '../../images/purple-waves-bottom.svg');
+        wavesBottomCrests.setAttribute('alt', 'downward wave crests');
 
-class DonateYellowPurple extends HTMLElement {
-    connectedCallback(){
-        this.innerHTML = 
-        `<section class="waves__container">
-        <img class="waves__crests top-waves" src="../../images/yellow-waves-top.svg" alt="upward wave crests"> 
-        <div class="waves__body waves__yellow">
-            <div class="divider__content">
-                <h2 class="divider__text">Help Make the Adventures Happen</h2>
-                <a class="button button__purple" href="https://paypal.me/platypusadventures?country.x=US&locale.x=en_US">Donate</a>   
-            </div>
-        </div>
-        <img class="waves__crests bottom-waves" src="../../images/yellow-waves-bottom.svg" alt="downward wave crests"> 
-    </section>`
-    }
-}
+        const wavesBody = document.createElement('div');
+        wavesBody.setAttribute('class', 'waves__body waves__purple');
+        const wavesContent = document.createElement('article');
+        wavesContent.setAttribute('class', 'intro__content');
+        
+        const introTitle = document.createElement('h1');
+        introTitle.setAttribute('class', 'intro__title');
+        introTitle.textContent = this.getAttribute('intro-title')
 
-class VolunteerBlueYellow extends HTMLElement {
-    connectedCallback(){
-        this.innerHTML = 
-        `<section class="waves__container">
-        <img class="waves__crests top-waves" src="../../images/blue-waves-top.svg" alt="upward wave crests"> 
-        <div class="waves__body waves__blue">
-            <div class="divider__content">
-                <h2 class="divider__text">Become Part of the Adventure</h2>
-                <a class="button button__yellow" href="/get-involved/volunteering">Volunteer</a>   
-            </div>
-        </div>
-        <img class="waves__crests bottom-waves" src="../../images/blue-waves-bottom.svg" alt="downward wave crests"> 
-    </section>`
-    }
-}
+        const introText = document.createElement('p');
+        introText.setAttribute('class', 'intro__text');
+        introText.textContent = this.getAttribute('intro-text')
 
-class ReadGreenPurple extends HTMLElement {
-    connectedCallback(){
-        this.innerHTML = 
-        `<section class="waves__container">
-        <img class="waves__crests top-waves" src="../../images/green-waves-top.svg" alt="upward wave crests"> 
-        <div class="waves__body waves__green">
-            <div class="divider__content">
-                <h2 class="divider__text">A Story with Every Adventure</h2>
-                <a class="button button__purple" href="/platypus-stories">Read Ours</a>   
-            </div>
-        </div>
-        <img class="waves__crests bottom-waves" src="../../images/green-waves-bottom.svg" alt="downward wave crests"> 
-    </section>`
-    }
-}
-
-class ReadPurpleGreen extends HTMLElement {
-    connectedCallback(){
-        this.innerHTML = 
-        `<section class="waves__container">
-        <img class="waves__crests top-waves" src="../../images/purple-waves-top.svg" alt="upward wave crests"> 
-        <div class="waves__body waves__purple">
-            <div class="divider__content">
-                <h2 class="divider__text">A Story with Every Adventure</h2>
-                <a class="button button__green" href="/platypus-stories">Read Ours</a>   
-            </div>
-        </div>
-        <img class="waves__crests bottom-waves" src="../../images/purple-waves-bottom.svg" alt="downward wave crests"> 
-    </section>`
+        wavesContent.appendChild(introTitle);
+        wavesContent.appendChild(introText);
+        wavesBody.appendChild(wavesContent);
+        introSection.appendChild(wavesTopCrests);
+        introSection.appendChild(wavesBody);
+        introSection.appendChild(wavesBottomCrests);
+        this.appendChild(introSection)
     }
 }
 
 
+class WavesDivider extends HTMLElement {
+    constructor(){
+        super();
+        const wavesSection = document.createElement('section');
+        wavesSection.setAttribute('class','waves__container');
+        const bkgdColor = this.getAttribute('background-color');
+        const btnColor = this.getAttribute('button-color');
+        const link = this.getAttribute('link');
 
-customElements.define('donate-green-purple', DonateGreenPurple);
-customElements.define('donate-yellow-purple', DonateYellowPurple);
-customElements.define('volunteer-blue-yellow', VolunteerBlueYellow);
-customElements.define('read-green-purple', ReadGreenPurple);
-customElements.define('read-purple-green', ReadPurpleGreen);
+        const wavesTopCrests = document.createElement('img');
+        wavesTopCrests.setAttribute('class', 'waves__crests top-waves');
+        wavesTopCrests.setAttribute('src', `../../images/${bkgdColor}-waves-top.svg`);
+        wavesTopCrests.setAttribute('alt', 'upward wave crests');
+        const wavesBottomCrests = document.createElement('img');
+        wavesBottomCrests.setAttribute('class', 'waves__crests bottom-waves');
+        wavesBottomCrests.setAttribute('src',`../../images/${bkgdColor}-waves-bottom.svg`);
+        wavesBottomCrests.setAttribute('alt', 'downward wave crests');
+
+        const wavesBody = document.createElement('div');
+        wavesBody.setAttribute('class',`waves__body waves__${bkgdColor}`);
+
+        const wavesContent = document.createElement('article');
+        wavesContent.setAttribute('class', 'divider__content');
+        
+        const dividerText = document.createElement('h2');
+        dividerText.setAttribute('class', 'divider__text');
+        dividerText.textContent = this.getAttribute('divider-text')
+
+        const dividerBtn = document.createElement('a');
+        dividerBtn.setAttribute('href', `${link}`);
+        dividerBtn.setAttribute('class', `button button__${btnColor}`);
+        dividerBtn.textContent = this.getAttribute('btn-text')
+
+        wavesContent.appendChild(dividerText);
+        wavesContent.appendChild(dividerBtn);
+        wavesBody.appendChild(wavesContent);
+        wavesSection.appendChild(wavesTopCrests);
+        wavesSection.appendChild(wavesBody);
+        wavesSection.appendChild(wavesBottomCrests);
+        this.appendChild(wavesSection)
+    }
+}
+
+
+
+customElements.define('intro-waves', IntroWaves);
+customElements.define('waves-divider', WavesDivider);
