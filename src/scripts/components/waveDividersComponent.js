778302733b9ef25ab913870672449a630b72f453
kalewine <kayla.r.alewine@gmail.com>
2022-03-23 11:35:31 -0400

    Add header images and dividers with paragraphs

--- src/scripts/components/waveDividersComponent.js
+++ src/scripts/components/waveDividersComponent.js
@@ -64,7 +64,9 @@ class WavesDivider extends HTMLElement {
         
         const dividerText = document.createElement('h2');
         dividerText.setAttribute('class', 'divider__text');
-        dividerText.textContent = this.getAttribute('divider-text')
+        dividerText.textContent = this.getAttribute('divider-text');
+
+        
 
         const dividerBtn = document.createElement('a');
         dividerBtn.setAttribute('href', `${link}`);
@@ -72,6 +74,12 @@ class WavesDivider extends HTMLElement {
         dividerBtn.textContent = this.getAttribute('btn-text')
 
         wavesContent.appendChild(dividerText);
+        if(this.hasAttribute('divider-paragraph')){
+            const dividerParagraph = document.createElement('p');
+            dividerParagraph.setAttribute('class', 'waves__paragraph')
+            dividerParagraph.textContent = this.getAttribute('divider-paragraph');
+            wavesContent.appendChild(dividerParagraph);
+        }
         wavesContent.appendChild(dividerBtn);
         wavesBody.appendChild(wavesContent);
         wavesSection.appendChild(wavesTopCrests);
@@ -81,7 +89,42 @@ class WavesDivider extends HTMLElement {
     }
 }
 
+class HeaderWaves extends HTMLElement {
+    constructor(){
+        super();
+        const headerWaves = document.createElement('section');
+        headerWaves.setAttribute('class','header__waves');
+        
+        const wavesBottomCrests = document.createElement('img');
+        wavesBottomCrests.setAttribute('class', 'waves__crests bottom-waves');
+        wavesBottomCrests.setAttribute('src', '../../images/purple-waves-bottom.svg');
+        wavesBottomCrests.setAttribute('alt', 'downward wave crests');
+        
+        const wavesBody = document.createElement('div');
+        wavesBody.setAttribute('class', 'waves__body waves__purple');
+        wavesBody.setAttribute('id','header-waves');
+        
+        const wavesContent = document.createElement('article');
+        wavesContent.setAttribute('class', 'intro__content');
+        
+        const introTitle = document.createElement('h1');
+        introTitle.setAttribute('class', 'intro__title');
+        introTitle.textContent = this.getAttribute('intro-title')
+
+        const introText = document.createElement('p');
+        introText.setAttribute('class', 'intro__text');
+        introText.textContent = this.getAttribute('intro-text');
+
+        wavesContent.appendChild(introTitle);
+        wavesContent.appendChild(introText);
+        wavesBody.appendChild(wavesContent);
+        headerWaves.appendChild(wavesBody);
+        headerWaves.appendChild(wavesBottomCrests);
+        this.appendChild(headerWaves)
+    }
+}
 
 
 customElements.define('intro-waves', IntroWaves);
 customElements.define('waves-divider', WavesDivider);
+customElements.define('header-waves', HeaderWaves);
