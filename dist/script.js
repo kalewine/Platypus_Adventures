8e075cba1dd58162e478c135825c08ade40905ef
kalewine <kayla.r.alewine@gmail.com>
2024-03-19 12:23:55 -0400

    Remove platypus yoga

--- dist/script.js
+++ dist/script.js
@@ -56,7 +56,7 @@ class PlatypusFooter extends HTMLElement{connectedCallback(){this.innerHTML=`  <
                         </div>
                         <ul class="nav__dropdown" id="work">
                             <li  class="nav__dropdown-item"><a href="/platypus-work/tri-my-best" class="nav__dropdown-link">Tri My Best</a></li>
-                            <li  class="nav__dropdown-item" onfocusout="closeSubMenu('work')"><a href="/platypus-work/platypus-yoga" class="nav__dropdown-link" >Yoga</a></li>
+                            <li  class="nav__dropdown-item" onfocusout="closeSubMenu('work')"><a href="/platypus-work/platypus-yoga" class="nav__dropdown-link hidden" >Yoga</a></li> 
                         </ul>
                         <div class="nav__dropdown-shadow" id="work-shadow"></div>
                     </li>
